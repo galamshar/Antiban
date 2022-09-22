b7c78ed8117feeceb424061570852f67c515507a
galamsharx <g.myrzagalyev@gmail.com>
2022-09-22 15:18:13 +0600

    8/8 testing success

--- Antiban/Antiban.cs
+++ Antiban/Antiban.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Antiban
 {
     public class Antiban
     {
+        private List<EventMessage> Messages { get; } = new();
+
         /// <summary>
         /// Добавление сообщений в систему, для обработки порядка сообщений
         /// </summary>
@@ -11,6 +15,34 @@ namespace Antiban
         public void PushEventMessage(EventMessage eventMessage)
         {
             //TODO
+            var lastSendedNumber = Messages.LastOrDefault(m => m.DateTime <= eventMessage.DateTime);
+            var lastMessageToNumber = Messages.LastOrDefault(m => m.Phone == eventMessage.Phone && m.DateTime <= eventMessage.DateTime);
+
+            var tempDateTime = eventMessage.DateTime;
+
+            if (lastSendedNumber != null && lastSendedNumber.DateTime.AddSeconds(10) >= eventMessage.DateTime) tempDateTime = lastSendedNumber.DateTime.AddSeconds(10);
+            if (lastMessageToNumber != null && lastMessageToNumber.DateTime.AddMinutes(1) >= eventMessage.DateTime) tempDateTime = lastMessageToNumber.DateTime.AddMinutes(1);
+
+            if (eventMessage.Priority == 1)
+            {
+                var lastPriorityMessageToNumber =
+                    Messages.LastOrDefault(m => m.Phone == eventMessage.Phone && m.Priority == 1);
+
+                if (lastPriorityMessageToNumber != null && lastPriorityMessageToNumber.DateTime.AddHours(24) >= eventMessage.DateTime)
+                {
+                    tempDateTime = lastPriorityMessageToNumber.DateTime.AddHours(24);
+                }
+            }
+
+            while (Messages.Any(m => m.DateTime == tempDateTime))
+            {
+                tempDateTime = tempDateTime.AddSeconds(10);
+            }
+
+            eventMessage.DateTime = tempDateTime;
+            Messages.Add(eventMessage);
+
+
         }
 
         /// <summary>
@@ -21,13 +53,20 @@ namespace Antiban
         {
             //TODO
             //Example
-            var result = new List<AntibanResult>();
-            for (int i = 0; i < 10; i++)
+            var results = new List<AntibanResult>();
+
+            foreach (var message in Messages)
             {
-                result.Add(new AntibanResult());
+                var result = new AntibanResult()
+                {
+                    EventMessageId = message.Id,
+                    SentDateTime = message.DateTime
+                };
+
+                results.Add(result);
             }
-            
-            return result;
+
+            return results.OrderBy(x => x.SentDateTime).ToList();
         }
     }
 }
